b925134c03b9334ca1e137273b1f464360ff08f4
hazemxiii <101417956+hazemxiii@users.noreply.github.com>
2026-03-03 21:46:31 +0200

    login_with_email

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserAuth } from '../services/auth.service';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(UserAuth);
   const router = inject(Router);
 
-  if (auth.isLogged()) {
-    return true
-  };
-
-  router.navigate(['/auth/login']);
-  return false;
-}
+  return auth.isLogged().pipe(
+    map((isLogged) => {
+      if (isLogged) {
+        return true;
+      }
+      router.navigate(['/auth/login']);
+      return false;
+    }),
+  );
+};
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,39 +3,93 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map, tap } from 'rxjs';
 import { IUser } from '../models/user.model';
 import { API } from '../../api/api';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  browserLocalPersistence,
+  setPersistence,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 @Injectable({ providedIn: 'root' })
 export class UserAuth {
   private http = inject(HttpClient);
   private url = `${API}/users`;
 
+  constructor() {}
+
   getUser(): IUser | null {
     const user = localStorage.getItem('user');
     return user ? JSON.parse(user) : null;
   }
 
-  isLogged = (): boolean => !!this.getUser();
-  isAuth = (): boolean => this.isLogged();
+  isLogged = (): Observable<boolean> => {
+    return new Observable<boolean>((subscriber) => {
+      const auth = getAuth();
+      const unsubscribe = onAuthStateChanged(
+        auth,
+        (user) => {
+          subscriber.next(!!user);
+          subscriber.complete();
+        },
+        (error) => {
+          subscriber.error(error);
+        },
+      );
+      return () => unsubscribe();
+    });
+  };
+  // isLogged = (): boolean => !!this.getUser();
+  // isAuth = (): boolean => this.isLogged();
   isAdmin = (): boolean => this.getUser()?.role === 'admin';
 
   login(email: string, password: string): Observable<IUser> {
-    return this.http.get<IUser[]>(`${this.url}?email=${email}&password=${password}`).pipe(
-      map((users) => {
-        if (!users.length) throw new Error('Invalid credentials');
-        return users[0];
-      }),
-      tap((user) => localStorage.setItem('user', JSON.stringify(user))),
-    );
+    const auth = getAuth();
+
+    return new Observable<IUser>((subscriber) => {
+      setPersistence(auth, browserLocalPersistence)
+        .then(() => {
+          return signInWithEmailAndPassword(auth, email, password);
+        })
+        .then((userCredential) => {
+          const user: IUser = {
+            id: 1,
+            name: userCredential.user?.displayName ?? '-',
+            email,
+            password,
+            role: 'user',
+          };
+
+          subscriber.next(user);
+          subscriber.complete();
+
+          console.log(auth.currentUser);
+        })
+        .catch((error) => {
+          console.error(error);
+          subscriber.error(error);
+        });
+    });
   }
 
   register(name: string, email: string, password: string): Observable<IUser> {
-    return this.http
-      .post<IUser>(this.url, { name, email, password, role: 'user' })
-      .pipe(tap((user) => localStorage.setItem('user', JSON.stringify(user))));
+    const userObservable = new Observable<IUser>((subscriber) => {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user: IUser = { id: 1, name, email, password, role: 'user' };
+          subscriber.next(user);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+    return userObservable;
   }
 
   logout(): void {
-    localStorage.removeItem('user');
+    signOut(getAuth());
   }
 }
